239e130ae6817696360f5f2dd7f6c12e1943708a
jeremy-ma <jeremy_ma@outlook.com>
2014-11-13 20:31:18 -0500

    included stats

--- scraper_funcs.py
+++ scraper_funcs.py
@@ -3,6 +3,9 @@ import Queue
 from collections import defaultdict
 import pdb
 import time
+import json
+
+key = '9213b085-ed48-4bf0-af91-d8fa519e3b35'
 
 def wait(w):
     while not w.can_make_request():
@@ -20,54 +23,58 @@ def grab_matches(seedid=43731318, num_matches=100):
 	returns:
 	A dictionary with 
 	keys: matchid(int) 
-	values: dict(keys:int(teamid 100 or 200), values: list of summoner ids for that team)
-	'''
+	values: tuple[0] dict(keys:int(teamid 100 or 200), values: list of summoner ids for that team)
+			tuple[1] teamid of winning team
+	'''	
 
 
 
-	key = '9213b085-ed48-4bf0-af91-d8fa519e3b35'
+
 	w = RiotWatcher(key)
 
 	sid_queue = Queue.Queue()
-
-	num_matchIds = 20
 	count = 0
 
 	matches_summoners = {}
 	seen_summoners = set()
 	seen_summoners.add(seedid)
 
-	while(count < num_matchIds):
-		if (w.can_make_request()):
+	while(count < num_matches):
 
-			m_hist_dict = w.get_match_history(summoner_id=seedid, ranked_queues='RANKED_SOLO_5x5')
-			match_list = m_hist_dict['matches']
+		wait(w)
+		m_hist_dict = w.get_match_history(summoner_id=seedid, ranked_queues='RANKED_SOLO_5x5')
+		match_list = m_hist_dict['matches']
 
-			# pdb.set_trace()
-			for match_record in match_list:
-				if match_record['matchMode'] == 'CLASSIC' and match_record['matchType'] == 'MATCHED_GAME':
-					matchid = match_record['matchId']
-					if matchid not in matches_summoners:
-					# use the other API call to get the participants..
-						wait(w)
-						match = w.get_match(matchid)
-						team_summonerids = defaultdict(list)
-						ptp_teamId = {}
-						for participant in match['participants']:
-							ptp_teamId[participant['participantId']] = participant['teamId']
+		# pdb.set_trace()
+		for match_record in match_list:
+			if match_record['matchMode'] == 'CLASSIC' and match_record['matchType'] == 'MATCHED_GAME':
+				matchid = match_record['matchId']
+				if matchid not in matches_summoners:
+				# use the other API call to get the participants..
+					wait(w)
+					match = w.get_match(matchid)
+					team_summonerids = defaultdict(list)
+					ptp_teamId = {}
+					for participant in match['participants']:
+						ptp_teamId[participant['participantId']] = participant['teamId']
 
-						for ptp_idents in match['participantIdentities']:
-							team = ptp_teamId[ptp_idents['participantId']]
-							summoner_id = ptp_idents['player']['summonerId']
-							team_summonerids[team].append(summoner_id)
+					for ptp_idents in match['participantIdentities']:
+						team = ptp_teamId[ptp_idents['participantId']]
+						summoner_id = ptp_idents['player']['summonerId']
+						team_summonerids[team].append(summoner_id)
 
-							if summoner_id not in seen_summoners:
-								sid_queue.put(summoner_id)
-								seen_summoners.add(summoner_id)
+						if summoner_id not in seen_summoners:
+							sid_queue.put(summoner_id)
+							seen_summoners.add(summoner_id)
 
-						matches_summoners[matchid] = team_summonerids
+					for team in match['teams']:
+						if team['winner'] is True:
+							winning_team = team['teamId']
+							break
 
-						count += 1
+					matches_summoners[matchid] = (team_summonerids,winning_team)
+
+					count += 1
 
 		# an exception will be thrown if the queue is empty haha
 		seedid = sid_queue.get()
@@ -76,6 +83,42 @@ def grab_matches(seedid=43731318, num_matches=100):
 
 	return matches_summoners
 
+def include_stats(matches_summoners):
+
+	w = RiotWatcher(key)
+
+	for matchid, (team_lists,_) in matches_summoners.items():
+		for teamid, teamlist in team_lists.items():
+			for index, summoner_id in enumerate(teamlist):
+				# replace the summonerid with player stat summary
+				wait(w)
+				teamlist[index] = w.get_ranked_stats(summoner_id)
+
+	return matches_summoners
+
 
 if __name__ == "__main__":
-	grab_matches()
+
+	matches = grab_matches(num_matches=10)
+
+	matches = include_stats(matches)
+
+	with open('data.json','w') as fp:
+		json.dump(matches,fp)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
